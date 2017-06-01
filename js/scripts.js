d46a87d5c09fcd34c5fb2fb7b8c9702c281f0cd3
Esvethlynna Pangelinan and Witty Chang <witty@>
2017-06-01 10:02:22 -0700

    add store item and display it in the receipt

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-
 $(document).ready(function() {
   $('#blanks form').submit(function(event) {
     event.preventDefault();
     var usernameInput = $("input#username").val();
     var useraddressInput = $("input#useraddress").val();
+    var beverage = $("select#beverage").val();
 
     $('.name').hide();
     $('.address').hide();
+    $('.form-group').hide();
     $('#postcard').show();
     $('.username').text(usernameInput);
     $('.useraddress').text(useraddressInput);
-
+    $('.beverage').text(beverage);
   });
 });
